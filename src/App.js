0abb39ed05dc5fd84837b977f542030f39e3b27b
Cristina Román <cristinaroman05.cr@gmail.com>
2024-01-26 18:34:32 +0100

    corregida

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import "./App.css";
 import BankAccount from "./components/BankAccount/BankAccount";
 import BidderControl from "./components/BidderControl/BidderControl";
 function App() {
-  const [bidderList, setBidderList] = React.useState({
-    total: 0,
-  });
+
+  const [bidderList, setBidderList] = React.useState([100]);
+
   const addBid = () => {
-    const newBidderList = {
-      ...bidderList, 
-      total: bidderList.total +5,
-    };
-    setBidderList(newBidderList);
+    const amount = 5;
+    const lastElement = bidderList[bidderList.length - 1];
+    const newBidList = [...bidderList, lastElement + amount];
+    console.log(newBidList)
+    setBidderList(newBidList);
   };
+
+
   return (
     <div className="app">
       <h1>Cuenta bancaria:</h1>
       <BankAccount></BankAccount>
       <h1>Listado de pujas:</h1>
-      <ul>
-      {/* {bidderList.map(bidder => <li>{bidder}</li>)} */}
-      </ul>
+
+      {bidderList.map(bidder =><li>{bidder}</li>)}
+
       <h1>Pujadores:</h1>
       <BidderControl addBid={addBid} name="Pedro"></BidderControl>
       <BidderControl addBid={addBid} name="Gonzalo"></BidderControl>
